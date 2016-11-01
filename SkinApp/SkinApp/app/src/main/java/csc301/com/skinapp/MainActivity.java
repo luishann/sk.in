2bb3de4f56e9717bb882cea48994879519c2eccc
Sebastian Alvarado <sebastian.alvarado@mail.utoronto.ca>
2016-11-01 00:57:14 -0400

    Android App is pushed and Navigation Drawer is working, refactored all the other apps that we are not using

--- SkinApp/SkinApp/app/src/main/java/csc301/com/skinapp/MainActivity.java
+++ SkinApp/SkinApp/app/src/main/java/csc301/com/skinapp/MainActivity.java
@@ -1,23 +1,40 @@
 package csc301.com.skinapp;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends FragmentActivity {
+    /* Instance Variables that will be important for this class*/
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private CharSequence mTitle;
+    private String[]options = {"Journal", "Discover","Products","Progress"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.homescreen);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
+        //setSupportActionBar(toolbar);
+        //Setting the Floating button on the left
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +43,18 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //Link the objects to the corresponding UI elements
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        //Set the adapter of the ListView, to be populated
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.navbar_list_item,options));
+        //Set each element's onClickListener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        //Allow action bar app icon to toggle nav drawer
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
     }
 
     @Override
@@ -42,11 +71,71 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    /* The click listener for ListView in the navigation drawer */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swap fragments in the main content view */
+    private void selectItem(int position) {
+        // update the main content by replacing fragments
+        Fragment fragment = new OptionsFragment();
+        Bundle args = new Bundle();
+        args.putInt(OptionsFragment.ARG_OPTION_NUMBER, position);
+        fragment.setArguments(args);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
+
+        // update selected item and title, then close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(options[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
+
+
+    /**
+     * Fragment that appears in the "content_frame"
+     */
+    public static class OptionsFragment extends Fragment {
+        public static final String ARG_OPTION_NUMBER = "option_number";
+
+
+        public OptionsFragment() {
+            // Empty constructor required for fragment subclasses
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_journal, container, false);
+            int i = getArguments().getInt(ARG_OPTION_NUMBER);
+            String option = getResources().getStringArray(R.array.options_array)[i];
+
+            int imageId = getResources().getIdentifier(option.toLowerCase(Locale.getDefault()),
+                    "drawable", getActivity().getPackageName());
+            ((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
+            getActivity().setTitle(option);
+            return rootView;
+        }
     }
 }
