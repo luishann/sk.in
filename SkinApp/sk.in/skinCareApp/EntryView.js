049de64edb2224de72d58b5093dd9def49dd61fe
Anisha Rohra <anisha.rohra@mail.utoronto.ca>
2016-11-08 15:09:35 -0500

    working on switching to node

--- SkinApp/sk.in/skinCareApp/EntryView.js
+++ SkinApp/sk.in/skinCareApp/EntryView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, AppRegistry, TouchableHighlight } from 'react-native';
 
 export default class EntryView extends Component {
   static get defaultProps() {
@@ -8,10 +8,24 @@ export default class EntryView extends Component {
     };
   }
 
+  _randFunc() {
+    fetch("https://100.65.209.140:3000/users", {method: "GET"})
+    .then((response) => response.json())
+    .then((responseData) => {
+      console.log("got here");
+      console.log(responseData);
+    })
+    .done();
+  }
+
   render() {
+
     return (
       <View>
         <Text>This is an EntryView scene.</Text>
+        <TouchableHighlight onPress={this._randFunc}>
+          <Text>Test</Text>
+        </TouchableHighlight>
       </View>
     )
   }
