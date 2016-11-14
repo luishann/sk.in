337a1797fb9d154c3e00cb24027b7ffbd2c9dc12
radhika2896 <radhika.krishnan@mail.utoronto.ca>
2016-11-14 13:05:54 -0500

    added changes in gradle

--- SkinApp/sk.in/skinCareApp/android/app/src/main/java/com/skincareapp/MainApplication.java
+++ SkinApp/sk.in/skinCareApp/android/app/src/main/java/com/skincareapp/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import cn.mandata.react_native_mpchart.MPChartPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new MPChartPackage(),
           new RCTCameraPackage()
       );
     }
