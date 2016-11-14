d67bc7a45bc4170ec1d300018408511f7ffc3c29
radhika2896 <radhika.krishnan@mail.utoronto.ca>
2016-11-14 12:36:23 -0500

    added changes to Analytics

--- SkinApp/sk.in/skinCareApp/Analytics.js
+++ SkinApp/sk.in/skinCareApp/Analytics.js
@@ -1,39 +1,53 @@
-import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
-import BarChart from 'react-native-chart';
+'use strict';
 
-  const data = [
-      ['Oct 29, 2016', 1],
-      ['Oct 30, 2016', 3],
-      ['Oct 31, 2016', 2],
-      ['Nov 1, 2016', 2],
-      ['Nov 2, 2016', 3],
-      ['Nov 3, 2016', 4]
-  ];
+var React = require('react');
+var {
+	BarChart,
+	CombinedChart
+}=require('react-native-chart-android/index.android');
+var {
+  StyleSheet,
+  View,
+  Text
+} = require('react-native');
 
-  export default class Analytics extends Component {
-      render() {
-          return (
-              <View style={styles.container}>
-                  <BarChart
-                      style={styles.chart}
-                      data={data}
-                      yValues = {[1, 2, 3, 4, 5]}
-                      type="bar"
-                   />
-              </View>
-          );
-      }
-  }
-  const styles = StyleSheet.create({
-      container: {
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: 'white',
-      },
-      chart: {
-          width: 300,
-          height: 200,
-      },
-  });
+var Component = React.createClass({
+	getBarData:function (argument) {
+		var data={
+			xValues:['Oct 29, 2016','Oct 30, 2016','Oct 31, 2016', 'Nov 1, 2016', 'Nov 2, 2016', 'Nov 3, 2016'],
+			yValues:[
+				{
+					data:[1, 3, 2, 2, 3, 4],
+					config:{
+						color:'blue'
+					}
+				},
+			]
+		};
+		return data;
+	},
+	render: function() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.chartContainer}>
+					<BarChart style={{flex:1}} data={this.getBarData()}/>
+				</View>
+			</View>
+		);
+	}
+});
+
+var styles = StyleSheet.create({
+	container:{
+		flex:1
+	},
+	chartContainer:{
+		flex:1
+	},
+	chart:{
+		flex:1
+	}
+});
+
+
+module.exports = Component;
