2f3c79b95512d96d9902355b9c559c9f7ea9e391
radhika2896 <radhika.krishnan@mail.utoronto.ca>
2016-11-14 15:00:04 -0500

    added design to Product Analytics

--- SkinApp/sk.in/skinCareApp/ProductAnalytics.js
+++ SkinApp/sk.in/skinCareApp/ProductAnalytics.js
@@ -14,18 +14,18 @@ var {
 var Component = React.createClass({
 	getBarData:function (argument) {
 		var data={
-			xValues:['Aug-2016','Sept-2016','Oct-2016','Nov-2016'],
+			xValues:['Aug 2016','Sept 2016','Oct 2016','Nov 2016'],
 			yValues:[
 				{
 					data:[0,2,4,5],
-					label:'Time1',
+					label:'Tea Tree Face Wash',
 					config:{
 						color:'red'
 					}
 				},
 				{
 					data:[2,3,4,5],
-					label:'Time2',
+					label:'Clean N Clear',
 					config:{
 						color:'yellow'
 					}
@@ -38,7 +38,15 @@ var Component = React.createClass({
 		return (
 			<View style={styles.container}>
 				<View style={styles.chartContainer}>
-					<LineChart style={{flex:1}} data={this.getBarData()}/>
+					<LineChart
+						style={{flex:1}}
+						data={this.getBarData()}
+						visibleYRange={[0,5]}
+						drawGridBackground={false}
+						yAxisRight={{enable:false}}
+						yAxis={{startAtZero:false,drawGridLines:false}}
+						xAxis={{drawGridLines:false,position:"BOTTOM"}}
+					/>
 				</View>
 			</View>
 		);
