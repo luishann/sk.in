868f7400f36fd2c5d8374f6fff680d9f1dd717d1
Shannon Lui <shannonlui@hotmail.com>
2016-11-15 09:27:11 -0500

    deleted old app directory

--- SkinApp/example-react-native/index.ios.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import Root from './src/Root';
-// delete GLOBAL.XMLHttpRequest;
-console.disableYellowBox = true;
-
-// http://www18.online-convert.com/download-file/6bb0ae14fc16f672283f3fe4b211d909/converted-aec0e970.gif
-
-AppRegistry.registerComponent('MyAppointment', () => Root);
