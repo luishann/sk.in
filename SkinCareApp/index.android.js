06de063099c240870d97af9f92d355628a1abe35
sara golestaneh <sara.golestaneh@mail.utoronto.ca>
2016-12-01 22:12:21 -0500

    changed the return view in index

--- SkinCareApp/index.android.js
+++ SkinCareApp/index.android.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component, PropTypes } from 'react';
 import {
   AppRegistry,
@@ -20,14 +14,28 @@ import {
 } from 'react-native';
 
 import UserView from './UserView';
+import Signup from './Signup';
 
 export default class SkinCareApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // userID hardcoded to 1, later we should change it to null once we
+      // finished implementing login/signup
+      userID: 0,
+    };
+  }
+
+  _setUserID(id) {
+    this.setState({userID: id});
+  }
 
   render() {
 
     return (
       <View style={{flex: 1}}>
-        <UserView />
+        { this.state.userID ?  <UserView userID={this.state.userID} />
+          : <Signup setUserID={this._setUserID}/>}
       </View>
     );
   }
